7ee1bb45aadaa52499ddbbada3ac7a3457809844
vaclav-kubes <240644@vutbr.cz>
2025-04-03 15:48:54 +0200

    plotting first PSS - just test

---
+++ THE_CODE/test.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+
+data = np.array(0)
+data = np.load("D:\\Users\\User\\Documents\\MLF_project\\MPA-MLF_data\\Test\\0.npy", "r")
+
+print(data.shape )
+plt.imshow(data)
+plt.show()
